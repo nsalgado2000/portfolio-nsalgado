829529c6ac59bc59f627316e016528c60b85fbef
Nicolas <2000nsalgado@gmail.com>
2024-12-25 14:56:20 -0300

    style: Identidade visual bem estabelecida

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,25 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'media',
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        heading: ["var(--font-poppins)"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: '#7C3AED', // purple-600
+          hover: '#6D28D9', // purple-700
+        },
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          '2xl': '1400px',
+        },
       },
     },
   },
